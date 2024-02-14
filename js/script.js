c1a07023e72ff5ae23d1da622e1e8962fbf82941
anasait7 <160064605+anasait7@users.noreply.github.com>
2024-02-14 22:23:59 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,1 @@
-const menu = document.getElementById("menu");
-const actions = document.getElementById("actions");
-
-menu.addEventListener("click",()=>{
-    hundleMenu();
-})
-
-function hundleMenu(){
-    menu.classList.toggle("is-active");
-    actions.classList.toggle("is-active");
-}
-
 
