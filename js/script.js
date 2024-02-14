48a0303974368ef3567e7fb0065d7ae7dfba513f
anasait7 <160064605+anasait7@users.noreply.github.com>
2024-02-14 21:54:15 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const menu = document.getElementById("menu");
+const actions = document.getElementById("actions");
+
+menu.addEventListener("click",()=>{
+    hundleMenu();
+})
+
+function hundleMenu(){
+    menu.classList.toggle("is-active");
+    actions.classList.toggle("is-active");
+}
+
+
